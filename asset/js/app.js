fd7cf093e9628729b4d4c5793885230db0129077
ahmed ali <ahmedalirajpootbhatti@gmail.com>
2022-10-03 04:55:11 -0700

    Full Responsive site Prixma

---
+++ asset/js/app.js
@@ -0,0 +1,42 @@
+$('#project_carousel').owlCarousel({
+    loop:true,
+    margin:24,
+    dots:false,
+    nav:false,
+    smartspeed:1000,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$('#reviews_carousel').owlCarousel({
+    loop:true,
+    margin:24,
+    nav:false,
+    smartspeed:1000,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
